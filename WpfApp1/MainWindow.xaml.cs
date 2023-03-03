d6749ca0d2ad062a366c8ef640dbaf22535711be
monusbence <72020098+monusbence@users.noreply.github.com>
2023-03-03 11:50:24 +0100

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNev_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+            string nev =  txtNev.Text;
+            string megjelenes = txtMegjelenes.Text;
+            Nyelv xy = new Nyelv();
+            StreamWriter
+        }
+    }
+}
---
+++ WpfApp1/Nyelv.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppPrgNYelvek
+{
+    public enum Nyelvcsaladok { moduláris, strukturált, funkcionális, objektumorientált };
+
+    public class Nyelv
+    {
+        string neve;
+        bool forditosE;
+        int megjelenesEve;
+        bool alacsonyszintuE;
+        int nepszeruseg2022;  //
+        Nyelvcsaladok nyelvcsalad;
+        public Nyelv(string neve,
+                     bool forditosE,
+                     int megjelenesEve,
+                     bool alacsonyszintuE,
+                     int nepszeruseg2022,
+                     Nyelvcsaladok nyelvcsalad)
+        {
+            this.neve = neve;
+            this.forditosE = forditosE;
+            this.megjelenesEve = megjelenesEve;
+            this.alacsonyszintuE = alacsonyszintuE;
+            this.nepszeruseg2022 = nepszeruseg2022;
+            this.nyelvcsalad = nyelvcsalad;
+        }
+        public string Neve { get => neve; }
+        public bool ForditosE { get => forditosE; }
+        public int MegjelenesEve { get => megjelenesEve; }
+        public bool AlacsonyszintuE { get => alacsonyszintuE; }
+        public int Nepszeruseg2022 { get => nepszeruseg2022; }
+        public Nyelvcsaladok Nyelvcsalad { get => nyelvcsalad; }
+        public bool NepszeruE()
+        {
+            return this.Nepszeruseg2022 < 4;
+        }
+
+    }
+}
